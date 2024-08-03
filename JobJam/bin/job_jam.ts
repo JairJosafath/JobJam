739f72a971b55c37ea357f14f81c4087a4257c70
jair josafath <jairjosafath@gmail.com>
2024-08-03 09:22:39 +0200

    added termination protection for dev and added a test resource to test environment management scheme

--- JobJam/bin/job_jam.ts
+++ JobJam/bin/job_jam.ts
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
+/**
+ * Understanding the Stack construct
+ * https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.Stack.html#class-stack-construct
+ * https://docs.aws.amazon.com/cdk/v2/guide/stacks.html
+ *
+ */
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { JobJamStack } from "../lib/job_jam-stack";
 
 const app = new cdk.App();
 new JobJamStack(app, "JobJamStack-dev", {
+	description: "JobJam stack for dev environment",
 	tags: {
 		env: "dev",
 		project: "jobjam",
@@ -12,8 +19,10 @@ new JobJamStack(app, "JobJamStack-dev", {
 });
 
 new JobJamStack(app, "JobJamStack-prod", {
+	description: "JobJam stack for prod environment",
 	tags: {
 		env: "prod",
 		project: "jobjam",
 	},
+	terminationProtection: true,
 });
