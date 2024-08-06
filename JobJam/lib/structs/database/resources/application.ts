f08f0108fecfe4192c7df621025259b1d37eb8e5
jair josafath <jairjosafath@gmail.com>
2024-08-07 01:35:59 +0200

    refactored test file names to be more understandable, added a resource policy on the restapi

---
+++ JobJam/lib/structs/database/resources/application.ts
@@ -0,0 +1,140 @@
+import {
+	AuthorizationType,
+	AwsIntegration,
+	CognitoUserPoolsAuthorizer,
+	PassthroughBehavior,
+	RequestAuthorizer,
+	RestApi,
+} from "aws-cdk-lib/aws-apigateway";
+import { TableV2 } from "aws-cdk-lib/aws-dynamodb";
+import { Construct } from "constructs";
+import { Function, Runtime, Code } from "aws-cdk-lib/aws-lambda";
+import path = require("path");
+import { UserPool } from "aws-cdk-lib/aws-cognito";
+import {
+	Effect,
+	PolicyDocument,
+	PolicyStatement,
+	Role,
+	ServicePrincipal,
+} from "aws-cdk-lib/aws-iam";
+import { vtlSerializer } from "../../../utils/vtl";
+import { Application } from "aws-cdk-lib/aws-appconfig";
+
+export class ApplicationResource extends Construct {
+	constructor(
+		scope: Construct,
+		id: string,
+		api: RestApi,
+		userPool: UserPool,
+		clientId: string,
+		dynamoDBTable: TableV2
+	) {
+		super(scope, id);
+		const authorizer = new CognitoUserPoolsAuthorizer(this, "PoolAuthorizer", {
+			cognitoUserPools: [userPool],
+		});
+
+		const applicationResource = api.root.addResource("applications");
+
+		const createApplicationRole = new Role(this, "CreateApplicationRole", {
+			assumedBy: new ServicePrincipal("apigateway.amazonaws.com"),
+			description: "Role for applicant to apply for job",
+			inlinePolicies: {
+				createApplicationPolicy: new PolicyDocument({
+					statements: [
+						new PolicyStatement({
+							actions: ["dynamodb:PutItem"],
+							resources: [dynamoDBTable.tableArn],
+						}),
+					],
+				}),
+			},
+		});
+
+		const applyMethod = applicationResource.addMethod(
+			"POST",
+			new AwsIntegration({
+				service: "dynamodb",
+				action: "PutItem",
+				options: {
+					credentialsRole: createApplicationRole,
+					requestTemplates: {
+						"application/json": vtlSerializer({
+							TableName: dynamoDBTable.tableName,
+							Item: {
+								pk: {
+									S: "Job#$input.path('$.jobId')",
+								},
+								sk: {
+									S: "Application#$context.requestId",
+								},
+								ApplicationId: {
+									S: "$context.requestId",
+								},
+								JobId: {
+									S: "$input.path('$.jobId')",
+								},
+
+								ApplicantId: {
+									S: "#if($context.authorizer.claims.sub)$context.authorizer.claims.sub#{else}unauthenticated#end",
+								},
+								ApplicantEmail: {
+									S: "#if($context.authorizer.claims.email)$context.authorizer.claims.sub#{else}unauthenticated#end",
+								},
+								Status: {
+									S: "PENDING",
+								},
+								Resume: {
+									S: "$input.path('$.resume')",
+								},
+								CoverLetter: {
+									S: "$input.path('$.coverLetter')",
+								},
+								Contact: {
+									M: {
+										email: {
+											S: "$input.path('$.contact.email')",
+										},
+										phone: {
+											S: "$input.path('$.contact.phone')",
+										},
+									},
+								},
+								datecreated: {
+									S: "$context.requestTime",
+								},
+								lastupdated: {
+									S: "$context.requestTime",
+								},
+							},
+						}),
+					},
+					passthroughBehavior: PassthroughBehavior.NEVER,
+					integrationResponses: [
+						{
+							statusCode: "200",
+							responseTemplates: {
+								"application/json": JSON.stringify({
+									applicationId: "$context.requestId",
+								}),
+							},
+						},
+						{
+							selectionPattern: "4\\d{2}",
+							statusCode: "400",
+						},
+						{
+							selectionPattern: "5\\d{2}",
+							statusCode: "500",
+						},
+					],
+				},
+			}),
+			{
+				authorizationType: AuthorizationType.COGNITO,
+				authorizer,
+			}
+		);
+	}
+}
---
+++ JobJam/test/applicant/apply.test.ts
@@ -0,0 +1,79 @@
+import { log } from "console";
+import { config } from "dotenv";
+
+config();
+
+const endpoint = process.env.ENDPOINT;
+
+test("authenticated user can apply to a job", async () => {
+	const loginResponse = await fetch(`${endpoint}/login`, {
+		method: "POST",
+		body: JSON.stringify({
+			username: process.env.TEST_APPLICANT_EMAIL,
+			password: process.env.TEST_APPLICANT_PASSWORD,
+		}),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+
+	const loginJson = await loginResponse.json();
+
+	expect(loginResponse.status).toBe(200);
+
+	if (loginResponse.status !== 200) {
+		console.log(loginJson);
+	}
+
+	const applyResponse = await fetch(`${endpoint}/applications`, {
+		method: "POST",
+		body: JSON.stringify({
+			jobId: "48ebb15e-3f26-4d83-bedb-303e9f47c677",
+			resume: "https://example.com/resume",
+			coverLetter: "I am a great fit for this job",
+			contact: {
+				email: process.env.TEST_APPLICANT_EMAIL,
+				phone: "1234567890",
+			},
+		}),
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: loginJson.AuthenticationResult.IdToken,
+		},
+	});
+
+	const applyJson = await applyResponse.json();
+
+	if (applyResponse.status !== 200) {
+		console.log(applyJson);
+	}
+
+	expect(applyResponse.status).toBe(200);
+});
+
+test("unauthenticated user cannot apply to a job", async () => {
+	const applyResponse = await fetch(`${endpoint}/applications`, {
+		method: "POST",
+		body: JSON.stringify({
+			jobId: "1",
+			resume: "https://example.com/resume",
+			coverLetter:
+				"I am also a great fit for this job but I dont want to make a profile",
+			contact: {
+				email: process.env.TEST_APPLICANT_EMAIL,
+				phone: "1234567890",
+			},
+		}),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+
+	const applyJson = await applyResponse.json();
+
+	if (applyResponse.status !== 200) {
+		console.log(applyJson);
+	}
+
+	expect(applyResponse.status).toBe(200);
+});
